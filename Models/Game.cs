d8a607bc97503a342801ec3a75de5ad21458db79
Somewhere <likeaboss13371@gmail.com>
2024-06-10 20:33:54 +0300

    Models Added

---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int Player1Id { get; set; }
+        public int Player2Id { get; set; }
+        public int? WinnerId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe.Models
+{
+    public class Rating
+    {
+        public int PlayerId { get; set; }
+        public int TotalGames { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
+    }
+}
