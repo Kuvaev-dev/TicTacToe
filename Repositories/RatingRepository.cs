4770344e4d3b20e576686a64a23e6e8701b917c3
Somewhere <likeaboss13371@gmail.com>
2024-06-10 22:01:06 +0300

    Repositories Added

---
+++ Repositories/RatingRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TicTacToe.Models;
+
+namespace TicTacToe.Repositories
+{
+    public class RatingRepository : IRepository<Rating>
+    {
+        private readonly string _connectionString;
+        public RatingRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public Rating GetById(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Ratings WHERE PlayerId = @PlayerId", connection);
+                command.Parameters.AddWithValue("@PlayerId", id);
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Rating
+                        {
+                            PlayerId = (int)reader["PlayerId"],
+                            Wins = (int)reader["Wins"],
+                            Losses = (int)reader["Losses"],
+                            Draws = (int)reader["Draws"]
+                        };
+                    }
+                }
+                return null;
+            }
+        }
+        public IEnumerable<Rating> GetAll()
+        {
+            var ratings = new List<Rating>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Ratings", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ratings.Add(new Rating
+                        {
+                            PlayerId = (int)reader["PlayerId"],
+                            Wins = (int)reader["Wins"],
+                            Losses = (int)reader["Losses"],
+                            Draws = (int)reader["Draws"]
+                        });
+                    }
+                }
+            }
+            return ratings;
+        }
+        public void Add(Rating rating)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO Ratings (PlayerId, Wins, Losses, Draws) VALUES (@PlayerId, @Wins, @Losses, @Draws)", connection);
+                command.Parameters.AddWithValue("@PlayerId", rating.PlayerId);
+                command.Parameters.AddWithValue("@Wins", rating.Wins);
+                command.Parameters.AddWithValue("@Losses", rating.Losses);
+                command.Parameters.AddWithValue("@Draws", rating.Draws);
+                command.ExecuteNonQuery();
+            }
+        }
+        public void Update(Rating rating)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("UPDATE Ratings SET Wins = @Wins, Losses = @Losses, Draws = @Draws WHERE PlayerId = @PlayerId", connection);
+                command.Parameters.AddWithValue("@PlayerId", rating.PlayerId);
+                command.Parameters.AddWithValue("@Wins", rating.Wins);
+                command.Parameters.AddWithValue("@Losses", rating.Losses);
+                command.Parameters.AddWithValue("@Draws", rating.Draws);
+                command.ExecuteNonQuery();
+            }
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException("Delete method is not implemented for RatingRepository");
+        }
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TicTacToe.Models;
+
+namespace TicTacToe.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public User GetById(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Users WHERE Id = @Id AND IsDeleted = 0", connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new User
+                        {
+                            Id = (int)reader["Id"],
+                            Username = (string)reader["Username"],
+                            Password = (string)reader["Password"],
+                            IsDeleted = (bool)reader["IsDeleted"],
+                            LastLogin = reader["LastLogin"] as DateTime?
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var users = new List<User>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Users WHERE IsDeleted = 0", connection);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(new User
+                        {
+                            Id = (int)reader["Id"],
+                            Username = (string)reader["Username"],
+                            Password = (string)reader["Password"],
+                            IsDeleted = (bool)reader["IsDeleted"],
+                            LastLogin = reader["LastLogin"] as DateTime?
+                        });
+                    }
+                }
+            }
+            return users;
+        }
+
+        public void Add(User user)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO Users (Username, Password, IsDeleted, LastLogin) VALUES (@Username, @Password, @IsDeleted, @LastLogin)", connection);
+                command.Parameters.AddWithValue("@Username", user.Username);
+                command.Parameters.AddWithValue("@Password", user.Password);
+                command.Parameters.AddWithValue("@IsDeleted", user.IsDeleted);
+                command.Parameters.AddWithValue("@LastLogin", (object)user.LastLogin ?? DBNull.Value);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("UPDATE Users SET Username = @Username, Password = @Password, IsDeleted = @IsDeleted, LastLogin = @LastLogin WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", user.Id);
+                command.Parameters.AddWithValue("@Username", user.Username);
+                command.Parameters.AddWithValue("@Password", user.Password);
+                command.Parameters.AddWithValue("@IsDeleted", user.IsDeleted);
+                command.Parameters.AddWithValue("@LastLogin", (object)user.LastLogin ?? DBNull.Value);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("UPDATE Users SET IsDeleted = 1 WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
