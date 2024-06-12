0843c9fb4742f1bd0ef8867fb4cc22d3dc687de2
Somewhere <likeaboss13371@gmail.com>
2024-06-12 15:33:15 +0300

    Ratings Page Added

---
+++ Views/RatingPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using TicTacToe.Repositories;
+using TicTacToe.Services;
+
+namespace TicTacToe.Views
+{
+    public partial class RatingPage : Page
+    {
+        private readonly PlayerService _playerService;
+
+        public RatingPage(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            var connectionString = "your_connection_string_here";
+            var playerRepository = new PlayerRepository(connectionString);
+            _playerService = new PlayerService(playerRepository);
+            Loaded += RatingPage_Loaded;
+        }
+
+        private void RatingPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                var topPlayers = _playerService.GetTopPlayers(5); // Получение топ-5 игроков
+                RatingDataGrid.ItemsSource = topPlayers; // Привязка к элементу управления для отображения рейтинга
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
