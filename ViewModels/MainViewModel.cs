105f5c24a77ff69355b452504fc65bafd2e01013
Somewhere <likeaboss13371@gmail.com>
2024-06-16 02:17:34 +0300

    Current Page Changed

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -29,7 +29,7 @@ namespace TicTacToe.ViewModels
         /// </summary>
         public MainViewModel()
         {
-            CurrentPage = new RatingPage(this);
+            CurrentPage = new LoginPage(this);
         }
 
         /// <summary>
